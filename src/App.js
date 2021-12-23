dbbb0af3259003f493da18ad1867c7464353b70d
bh10-d <duchieubui25012002@gmail.com>
2021-12-23 15:50:40 -0500

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = ()=>(
+  <div>
+    <h1>HATS PAGE</h1>  
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
-
+// exact mac dinh la true (exact cho duogn dan tuyet doi khi ma duong dan dung y nhu the thi moi vao duoc)
+// react-router-dom v6 chuyen Switch thanh Routes
+//cu phap react-router-dom v6 <Route exact path="/" element={<HomePage />} />
+//cu phap react-router-dom v6 tro xuong <Route exact path="/" component={HomePage} />
 export default App;
